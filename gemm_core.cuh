f8f5a8a9b9dadfcba7cfa55d1fa835e08ee2a14d
mutsuki <mutsuki@momo86.net>
2019-05-25 16:44:29 +0900

    Add pragma unroll

--- gemm_core.cuh
+++ gemm_core.cuh
@@ -9,9 +9,11 @@ template<>
 __device__ void gemm_core16x16<float, 1lu>(float* const c, const float* const a, const float* const b, const unsigned ldm, const unsigned unique_id){
 	const unsigned x = unique_id >> 1;
 	const unsigned y_start = (unique_id & 0x1) << 3;
+#pragma unroll
 	for(unsigned p = 0; p < 8; p++){
 		const auto y = y_start + p;
 		float sum = 0.0f;
+#pragma unroll
 		for(unsigned i = 0; i < 4; i++){
 			const float4 b4 = *reinterpret_cast<const float4*>(b  + x * ldm + i * 4);
 			sum += b4.x * a[y + (i * 4 + 0) * ldm];
@@ -30,9 +32,11 @@ template<>
 __device__ void gemm_tn_core16x16<float, 1lu>(float* const c, const float* const a, const float* const b, const unsigned ldm, const unsigned unique_id){
 	const unsigned x = unique_id >> 1;
 	const unsigned y_start = (unique_id & 0x1) << 3;
+#pragma unroll
 	for(unsigned p = 0; p < 8; p++){
 		const auto y = y_start + p;
 		float sum = 0.0f;
+#pragma unroll
 		for(unsigned i = 0; i < 4; i++){
 			const float4 b4 = *reinterpret_cast<const float4*>(b  + x * ldm + i * 4);
 			const float4 a4 = *reinterpret_cast<const float4*>(a  + y * ldm + i * 4);
