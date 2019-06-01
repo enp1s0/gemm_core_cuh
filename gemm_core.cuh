4c1f8a8f3c71adfe308c12da5136b3f163006cd6
mutsuki <mutsuki@momo86.net>
2019-06-01 13:15:12 +0900

    Add ldms for each matrices as argument

--- gemm_core.cuh
+++ gemm_core.cuh
@@ -4,10 +4,10 @@
 
 namespace mtk {
 template<class T, std::size_t num_warps> 
-__device__ inline void gemm_core16x16(T* const c, const T* const a, const T* const b, const unsigned ldm, const unsigned unique_id);
+__device__ inline void gemm_core16x16(T* const c, const unsigned ldm_c, const T* const a, const unsigned ldm_a, const T* const b, const unsigned ldm_b, const unsigned unique_id);
 
 template<>
-__device__ inline void gemm_core16x16<float, 1lu>(float* const c, const float* const a, const float* const b, const unsigned ldm, const unsigned unique_id){
+__device__ inline void gemm_core16x16<float, 1lu>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id){
 	const auto lane = unique_id >> 4;
 	const auto y = unique_id & 0xf;
 	// unrollするとレジスタを1つ多く確保する
@@ -18,9 +18,9 @@ __device__ inline void gemm_core16x16<float, 1lu>(float* const c, const float* c
 
 		// はじめにcを読んでおくと誤差が小さくなる
 		// cuBLASの内部と計算順序が合うのかも
-		float sum = c[x * ldm + y];
+		float sum = c[x * ldm_c + y];
 		for(unsigned k = 0; k < 16; k+=1){
-			sum = fmaf(a[y + ldm * k], b[x * ldm + k], sum);
+			sum = fmaf(a[y + ldm_a * k], b[x * ldm_b + k], sum);
 		}
 		// float4による128bitアクセスはSharedメモリに対しては意味がないみたい
 		/*for(unsigned k = 0; k < 16; k += 4){
@@ -30,12 +30,12 @@ __device__ inline void gemm_core16x16<float, 1lu>(float* const c, const float* c
 			sum = fmaf(a[(k + 2) * ldm + y], b4.z, sum);
 			sum = fmaf(a[(k + 3) * ldm + y], b4.w, sum);
 		}*/
-		c[x * ldm + y] = sum;
+		c[x * ldm_c + y] = sum;
 	}
 }
 
 template<>
-__device__ inline void gemm_core16x16<half, 1lu>(half* const c, const half* const a, const half* const b, const unsigned ldm, const unsigned unique_id){
+__device__ inline void gemm_core16x16<half, 1lu>(half* const c, const unsigned ldm_c, const half* const a, const unsigned ldm_a, const half* const b, const unsigned ldm_b, const unsigned unique_id){
 	const auto y = unique_id & 0xf;
 	const auto x = (unique_id >> 4) << 3;
 	unsigned i, k;
@@ -47,17 +47,17 @@ __device__ inline void gemm_core16x16<half, 1lu>(half* const c, const half* cons
 
 #pragma unroll
 	for(k = 0; k < 16; k+= 2){
-		const auto a2 = __halves2half2(a[k * ldm + y], a[(k + 1) * ldm + y]);
+		const auto a2 = __halves2half2(a[k * ldm_a + y], a[(k + 1) * ldm_a + y]);
 
-		const half2 *b2 = (half2*)(b + x * ldm + k);
+		const half2 *b2 = (half2*)(b + x * ldm_b + k);
 		for(i = 0; i < 8; i++){
 			sums[i] = __hfma2(a2, *(b2), sums[i]);
-			b2 += ldm / 2;
+			b2 += ldm_b / 2;
 		}
 	}
 	for(i = 0; i < 8; i++){
 		const auto sum = sums[i];
-		c[(x + i) * ldm + y] = __low2half(sum) + __high2half(sum) + (c[(x + i) * ldm + y]);
+		c[(x + i) * ldm_c + y] = __low2half(sum) + __high2half(sum) + (c[(x + i) * ldm_c + y]);
 	}
 }
 } // namespace mtk
